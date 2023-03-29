12150b3797956a9b1d5ab64005e035a2ccd45be1
hadeelsaf <122454154+hadeelsaf@users.noreply.github.com>
2023-03-29 20:50:42 +0300

    Add files via upload

---
+++ CreateUser/CreateUser.js
@@ -0,0 +1,29 @@
+// createUser send all user data to the server. Filling the user object with
+// received data. (Route in server: CreateUser)
+const createUser = async (fullName, id, mail, password) => {
+  const res = await axios.post('http://localhost:8000/', {
+    fullName: 'Hadeel Safadi',
+    id: '206438939',
+    mail: 'hadhad123s@hotmail.com',
+    password: 'had123sa',
+  });
+  console.log(res.data);
+};
+var relSignUp = function () {
+  var user = {
+      id :$("#id-input").val(),
+      mail : $("#mail-input").val(),
+      full_name : $("#full-name-input").val(),
+      password : $("#password-one-input").val()
+  };
+
+  local.rel.save('user', {
+    fullName : user.full_name,
+    password : user.password,
+    mail : user.mail,
+    id : user.mail
+  });
+};
+function Goto(){
+  location.replace("/Desktop/webapp2/todo_list.html");
+}
